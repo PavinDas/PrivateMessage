60ed2d6bf93627128a6091ecb0500645003cf7e6
Pavin Das <pavindas007@gmail.com>
2025-10-24 12:20:29 +0530

    New Message

---
+++ run.ps1
@@ -0,0 +1,5 @@
+git add .
+git commit -m "New message"
+git push origin master
+git pull origin master
+Write-Host "✅ Message Send Successfully!" -ForegroundColor Green
--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ git add .
 git commit -m "New Message"
 git push origin master
 git pull origin master
+echo -e "\e[32m✅ Message Send Successfully!\e[0m"
